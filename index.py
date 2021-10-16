cc173c37da99c06eceee195a725eb0363f21e791
Dusk Light <r.rahul.developer@gmail.com>
2021-10-16 00:04:40 +0000

    rules embed complete, pfp changes

--- index.py
+++ index.py
@@ -34,4 +34,36 @@ async def _talk(ctx, *, question, user: discord.Member = None):
 
     await ctx.send(answer_filtered)
 
+@bot.command(aliases = ["rules", "rule"])
+async def _rule(ctx):
+    embed = discord.Embed(
+        title = "Rules",
+        description = '''
+            Please keep the following rules in mind when talking.
+
+            ‣ Be respectful
+            ‣ No spamming 
+            ‣ Any form of racism, gore, etc are forbidden
+            ‣ Don't advertise without permission from staffs
+            ‣ Talks about religion and politics can be taken to DMs
+            ‣ Maintain our server standards
+
+            By staying in the community you agree with the given rules
+        ''',
+        color = discord.Color.blue()
+    )
+    embed.set_footer(
+        text = "Team Insomnia",
+        icon_url = "https://media.discordapp.net/attachments/898127603922239513/898816884621848616/yerOW6.jpg?width=524&height=393"
+    )
+
+    await ctx.send(embed = embed)
+
+@bot.command(aliases = ["rules_image", "rule_image", "rule_img"])
+async def _rule_image(ctx):
+    embed = discord.Embed()
+    embed.set_image(url = "https://media.discordapp.net/attachments/898436139164266507/898900778973032478/RULES.png")
+
+    await ctx.send(embed = embed)
+
 bot.run(token)
