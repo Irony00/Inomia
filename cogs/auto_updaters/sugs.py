8da83e827c8510e3ea00256aa05908a1ccdd61fb
ShadowRanger5 <53829601+namanko@users.noreply.github.com>
2021-11-17 08:18:48 +0530

    Create sugs.py

---
+++ cogs/auto_updaters/sugs.py
@@ -0,0 +1,21 @@
+21 lines (17 sloc)  1.27 KB
+   
+import discord
+from discord.ext import commands
+from discord.commands import slash_command
+import random
+
+easter = ["Keep the rules in mind and enjoy your time here ^-^","You can give your feedback abt the bot using the feedback command","You can get support here:- https://discord.gg/UYBKcTYzyU"]
+
+class SugsCog(commands.Cog):
+    def __init__(self, bot):
+      self.bot = bot
+
+    @commands.Cog.listener()
+    async def on_command_completion(self, ctx):
+      adPer = random.randrange(0, 30)
+      if adPer < 2:
+        await ctx.send(random.choice(easter))
+
+def setup(bot):
+    bot.add_cog(SugsCog(bot))
