9844be784deb9ddc7cf4d05c87c3ea791455e6fb
ShadowRanger5 <53829601+namanko@users.noreply.github.com>
2021-11-17 08:46:09 +0530

    Create Support.py

---
+++ cogs/features/Support.py
@@ -0,0 +1,20 @@
+import discord
+from discord.ext import commands
+
+class Support(commands.Cog):
+    def __init__(self, bot):
+      self.bot = bot
+      
+    @commands.command()
+    async def _feedback(self, *args):
+        feedback = " ".join(args)
+        user = ctx.message.author
+        await ctx.send('Your feedback has been sent!')
+        channel = self.bot.get_channel(910367070397534228)
+        msg = await channel.respond(f'**New Feedback by {user}:** {feedback}')
+        await msg.add_reaction('✅')
+        await msg.add_reaction('⛔')
+        await ctx.message.delete
+        
+def setup(bot):
+    bot.add_cog(Support(bot))
