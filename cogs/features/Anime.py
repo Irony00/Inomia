b7f9cdb259d1695e40ce76f4a7027a0ea5044e2a
PAUL-OP <69387148+PAUL-OP@users.noreply.github.com>
2021-11-18 11:22:31 +0530

    Update Anime.py

--- cogs/features/Anime.py
+++ cogs/features/Anime.py
@@ -126,7 +126,7 @@ class AnimeCog(commands.Cog):
     @commands.command()
     async def waifu(self, ctx):
         #im = random.choice(waifu)
-        waifu = Subreddit(random.choice(["AnimeGirls","Waifu","waifuism"]))
+        waifu = Subreddit("Animewallpaper")
         waifu.get_random()
         url = waifu.url
         title = waifu.title
