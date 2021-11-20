8ff3d7ffccfa43be7cd87699b8d988866c18be88
ShadowRanger5 <53829601+namanko@users.noreply.github.com>
2021-11-20 18:12:24 +0530

    Update snipe.py

--- cogs/features/snipe.py
+++ cogs/features/snipe.py
@@ -1,16 +1,15 @@
 import discord
 from discord.ext import commands
 
-snipe_message = {}
-
 class SnipeCog(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+        self.snipe = {}
 
     @commands.Cog.listener()
     async def on_message_delete(message):
-        snipe_message[message.channel.id]["author"] = message.author             
-        snipe_message[message.channel.id]["content"] = message.content
+        self.snipe[message.channel.id]["author"] = message.author             
+        self.snipe[message.channel.id]["content"] = message.content
         await sleep (60)
         del snipe_message[message.channel.id]
 
@@ -18,8 +17,8 @@ class SnipeCog(commands.Cog):
     async def snipe(ctx):
         channel = ctx.channel
         try: 
-            em = discord.Embed(name = f"gottem #{channel.name}", description = snipe_message[channel.id]["content"])
-            em.set_footer(text = f"this man {snipe_message[channel.id]["author"]}")
+            em = discord.Embed(name = f"gottem #{channel.name}", description = self.snipe[channel.id]["content"])
+            em.set_footer(text = f"this man {self.snipe[channel.id]["author"]}")
             await ctx.send(embed = em)
         except: 
             await ctx.send(f"There are no recent deleted messages in #{channel.name}")
