7c8c8128e0300228c7755f39ed90c26763eb36b5
Irony00 <r.rahul.developer@gmail.com>
2021-11-21 08:03:53 +0000

    Disabled invite easter for within Insomnia Server

--- cogs/auto_updaters/sugs.py
+++ cogs/auto_updaters/sugs.py
@@ -2,7 +2,8 @@ import discord
 from discord.ext import commands
 import random
 
-easter = ["The credit for the spicy hot waifu and husbando pics go to Tanjiro#3129, yami#2835, SALMON#8537. True legends :pray: ","Keep the rules in mind and enjoy your time here ^-^","You can give your feedback abt the bot using the feedback command","You can get support here:- https://discord.gg/UYBKcTYzyU"]
+outside_server_easters = ["You can get support here:- https://discord.gg/UYBKcTYzyU"]
+easter = ["The credit for the spicy hot waifu and husbando pics go to Tanjiro#3129, yami#2835, SALMON#8537. True legends :pray: ","Keep the rules in mind and enjoy your time here ^-^","You can give your feedback abt the bot using the feedback command"]
 
 class SugsCog(commands.Cog):
     def __init__(self, bot):
@@ -10,9 +11,15 @@ class SugsCog(commands.Cog):
 
     @commands.Cog.listener()
     async def on_command_completion(self, ctx):
-        adPer = random.randrange(0, 30)
-        if adPer < 2:
-            await ctx.send(random.choice(easter))
+      adPer = random.randrange(0, 30)
+      if adPer < 2:
+        easter_choice = random.choice(easter + outside_server_easters)
+        
+        insomnia_server_id = "898127603309899806"
+        if ctx.guild.id == insomnia_server_id and easter_choice in outside_server_easters:
+            return
+
+        await ctx.send(random.choice(easter))
 
 def setup(bot):
     bot.add_cog(SugsCog(bot))
